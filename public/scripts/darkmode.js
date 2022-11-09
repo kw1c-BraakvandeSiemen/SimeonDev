59274f002e5fe0c2267a3b5b86654d03296587a1
kw1c-BraakvandeSiemen <siemen.vandebraak@edu-kw1c.nl>
2022-11-09 15:42:29 +0100

    small fix animation consistency

--- public/scripts/darkmode.js
+++ public/scripts/darkmode.js
@@ -9,7 +9,7 @@ function toggleDarkMode() {
 	}
 	//maan
 	else {
-		document.getElementById("darkModeToggle").innerHTML = '<svg class="animate-[pulse_5s_cubic-bezier(0.4,_0,_0.6,_1)_infinite] h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>';
+		document.getElementById("darkModeToggle").innerHTML = '<svg class="animate-[pulse_3s_cubic-bezier(0.4,_0,_0.6,_1)_infinite] h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>';
 		localStorage.theme = "light";
 	}
 }
