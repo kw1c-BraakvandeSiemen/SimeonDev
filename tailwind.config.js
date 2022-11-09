7b78779c19a0ca71305bda13243ca8a921060039
kw1c-BraakvandeSiemen <siemen.vandebraak@edu-kw1c.nl>
2022-11-09 15:30:10 +0100

    animation fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	darkMode: "class",
-	content: ["public/*.html", "public/pages/*.html"],
+	content: ["public/*.html", "public/pages/*.html", "public/scripts/*.js"],
 	theme: {
 		extend: {},
 	},
