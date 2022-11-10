b75f0392e5bd3d5482583b7a6b5266737f312564
kw1c-BraakvandeSiemen <siemen.vandebraak@edu-kw1c.nl>
2022-11-10 10:39:56 +0100

    grammar

--- public/scripts/mobileNav.js
+++ public/scripts/mobileNav.js
@@ -6,31 +6,32 @@
 */
 
 //waarde declareren
-const btn = document.querySelector('.mobileMenuButton');
-const menu = document.querySelector('.mobileMenu');
+const btn = document.querySelector(".mobileMenuButton");
+const menu = document.querySelector(".mobileMenu");
 
 //toggled hidden zodat het wel zichtbaar word on click
 btn.addEventListener("click", () => {
-    menu.classList.toggle("hidden");
+	menu.classList.toggle("hidden");
 });
 
-//navbar verbergen op 
+//navbar verbergen op
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) 
+window.onscroll = function () {
+	var currentScrollPos = window.pageYOffset;
+	if (prevScrollpos > currentScrollPos) {
+		document.getElementById("navbar").style.top = "0";
+	} else {
+		document.getElementById("navbar").style.top = "-100px";
+	}
+	prevScrollpos = currentScrollPos;
+	//dont trigger when auto scrolling
+	if (prevScrollpos == 0) {
+		document.getElementById("navbar").style.top = "0";
+	}
+	/* disable on ios, had issue with scolling up and bounce back it trigger navbar animation on to fast of a scroll up
+  if (navigator.userAgent.match(/(iPod|iPhone|iPad)/))
   {
     document.getElementById("navbar").style.top = "0";
-  } 
-  else 
-  {
-    document.getElementById("navbar").style.top = "-100px";
   }
-  prevScrollpos = currentScrollPos;
-  //dont trigger when auto scrolling
-  if (prevScrollpos == 0) 
-  {
-    document.getElementById("navbar").style.top = "0";
-  }
-}
-
+  */
+};
