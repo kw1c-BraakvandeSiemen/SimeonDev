bc13991d7c32460e46d1f7ce94d38f43a209fd05
kw1c-BraakvandeSiemen <siemen.vandebraak@edu-kw1c.nl>
2024-05-01 11:15:12 +0200

    changes to the footer year now being automatised

---
+++ public/scripts/footer.js
@@ -0,0 +1,9 @@
+/*
+    Auteur:         <Siemen van de Braak>
+    Aanmaakdatum:   <01-05-2024>
+
+      JS footer
+*/
+
+// Footer jaartal
+document.getElementById("year").textContent = new Date().getFullYear();
